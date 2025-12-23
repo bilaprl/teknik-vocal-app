63d30cb8c4915297be810940ec85aa4596330db9
bilaprl <apriliantinabila04@gmail.com>
2025-12-23 19:12:11 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Teknik Vocal App - Konsep Lengkap",
+  title: "Teknik Vocal App - Rizva",
   description: "Aplikasi pembelajaran teknik vokal profesional",
 };
 
